0d7099a5a94a74287492439573a208a2d3b3338a
TetianaKubrak73 <tetianakubrak73@gmail.com>
2024-03-19 16:29:24 +0100

    4

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,10 +1,5 @@
 import * as contactsService from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
-import {
-  createContactSchema,
-  updateContactSchema,
-} from "../schemas/contactsSchemas.js";
-import { request } from "express";
 
 export const getAllContacts = async (req, res, next) => {
   try {
@@ -45,10 +40,6 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
     const { name, email, phone } = req.body;
     const result = await contactsService.addContact(name, email, phone);
 
@@ -60,11 +51,6 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
   try {
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
     const { id } = req.params;
     const result = await contactsService.updateContact(id, req.body);
     if (!result) {
