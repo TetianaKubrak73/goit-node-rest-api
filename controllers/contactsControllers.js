b42e0ae61eb35d22421b5da766f26acea2a3117e
TetianaKubrak73 <tetianakubrak73@gmail.com>
2024-03-19 16:51:57 +0100

    controllerdecorator

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,64 +1,45 @@
 import * as contactsService from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
+import { controllerDecorator } from "../helpers/controllerDecorator.js";
 
-export const getAllContacts = async (req, res, next) => {
-  try {
-    const result = await contactsService.listContacts();
+export const getAllContacts = controllerDecorator(async (req, res, next) => {
+  const result = await contactsService.listContacts();
 
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+  res.status(200).json(result);
+});
+
+export const getOneContact = controllerDecorator(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+
+  if (!result) {
+    throw HttpError(404, `Not found`);
   }
-};
+  res.status(200).json(result);
+});
 
-export const getOneContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsService.getContactById(id);
-
-    if (!result) {
-      throw HttpError(404, `Not found`);
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+export const deleteContact = controllerDecorator(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404, `Not found`);
   }
-};
+  res.status(200).json(result);
+});
 
-export const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsService.removeContact(id);
-    if (!result) {
-      throw HttpError(404, `Not found`);
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+export const createContact = controllerDecorator(async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const result = await contactsService.addContact(name, email, phone);
+
+  res.status(201).json(result);
+});
+
+export const updateContact = controllerDecorator(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await contactsService.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Not found`);
   }
-};
 
-export const createContact = async (req, res, next) => {
-  try {
-    const { name, email, phone } = req.body;
-    const result = await contactsService.addContact(name, email, phone);
-
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contactsService.updateContact(id, req.body);
-    if (!result) {
-      throw HttpError(404, `Not found`);
-    }
-
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(200).json(result);
+});
