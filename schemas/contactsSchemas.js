29ee434427a6be9d9d02b9ef18fe4b914b36967d
TetianaKubrak73 <tetianakubrak73@gmail.com>
2024-03-26 18:40:51 +0100

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,13 +4,14 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.string(),
+  favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
+  favorite: Joi.boolean(),
 })
   .min(1)
   .message("Body must have at least one field");
